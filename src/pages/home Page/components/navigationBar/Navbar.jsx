d12b7c7c25f8faf190ba0f1f56fe9b6aa226d769
djSomeone <gauravkumarprajapati496@gmail.com>
2024-12-25 23:59:42 +0530

    fixed navbar responsiveness

--- src/pages/home Page/components/navigationBar/Navbar.jsx
+++ src/pages/home Page/components/navigationBar/Navbar.jsx
@@ -1,22 +1,27 @@
-// Navbar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../../../../asset/logo.png'; // Import your logo image
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons'; // Import solid icons
-// import { faUser } from '@fortawesome/free-regular-svg-icons'; // Import regular icon
 import { Icon } from '@iconify/react';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar-container">
       <div className="navbar-content">
         <div className="navbar-logo">
           <a href="/">
-            <img src={logo} alt="Iris Fashion Logo" className="navbar-logo-img"/>
+            <img src={logo} alt="Iris Fashion Logo" className="navbar-logo-img" />
           </a>
         </div>
-        <ul className="navbar-nav">
+        <button className="navbar-toggle" onClick={toggleMenu}>
+          <Icon icon="ic:round-menu" /> {/* Menu Icon */}
+        </button>
+        <ul className={`navbar-nav ${isOpen ? 'show' : ''}`}>
           <li className="navbar-nav-item">
             <a href="/" className="navbar-nav-link active">
               Home
@@ -54,18 +59,17 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="navbar-icons">
-        <a href="#" className="navbar-icon">
-          <Icon icon="tabler:search" /> {/* Search */}
-        </a>
-        <a href="#" className="navbar-icon">
-          <Icon icon="iconamoon:profile-light" /> {/* Profile */}
-        </a>
-        <a href="#" className="navbar-icon">
-          <Icon icon="solar:bag-3-outline" /> {/* Cart */}
-        </a>
+          <a href="#" className="navbar-icon">
+            <Icon icon="tabler:search" /> {/* Search */}
+          </a>
+          <a href="#" className="navbar-icon">
+            <Icon icon="iconamoon:profile-light" /> {/* Profile */}
+          </a>
+          <a href="#" className="navbar-icon">
+            <Icon icon="solar:bag-3-outline" /> {/* Cart */}
+          </a>
+        </div>
       </div>
-      </div>
-      
     </nav>
   );
 };
