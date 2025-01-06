df5c93099444ed7294cf227744499cae1cc077cd
djSomeone <gauravkumarprajapati496@gmail.com>
2025-01-06 15:27:06 +0530

    fixed product cont in row in home page

--- src/pages/home Page/components/productsGrid/ProductGrid.jsx
+++ src/pages/home Page/components/productsGrid/ProductGrid.jsx
@@ -1,7 +1,7 @@
 // ProductGrid.jsx
 import React from 'react';
 import './ProductGrid.css';
-
+import { useNavigate } from 'react-router-dom';
 const dummyProducts = [
   {
     image: '/src/asset/test/product1.png', // Replace with actual image URLs
@@ -41,36 +41,46 @@ const dummyProducts = [
 //   },
 ];
 
+
+
+
 const ProductGrid = ({ title, description, products=dummyProducts }) => {
-return (
-    <div className="product-grid-container">
-        <div className="product-grid-header">
-            <h2>{title}</h2>
-            <p>{description}</p>
-        </div>
-        <br/>
-        <div className="product-grid">
-            {products.map((product, index) => (
-                <div className="product" key={index}>
-                    <div
-                        className=" product-image" 
-                        // style={{ backgroundImage: `url(${product.image})` }}
-                    ></div>
-                    <div className="product-details">
-                        <h3 className="product-name">{product.name}</h3>
-                        <div className="product-price">
-                            <span className="current-price">{product.price}₹</span>
-                            {product.originalPrice && (
-                                <span className="original-price">{product.originalPrice}₹</span>
-                            )}
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/product-details');
+  };
+
+  return (
+    <div className="product-grid-container" >
+      <div className="product-grid-header">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <br/>
+      <div className="product-grid">
+        {products.map((product, index) => (
+          <div className="product" key={index} onClick={handleClick}>
+            <div
+              className="product-image" 
+              // style={{ backgroundImage: `url(${product.image})` }}
+            ></div>
+            <div className="product-details">
+              <h3 className="product-name">{product.name}</h3>
+              <div className="product-price">
+                <span className="current-price">{product.price}₹</span>
+                {product.originalPrice && (
+                  <span className="original-price">{product.originalPrice}₹</span>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-);
+  );
 };
+
 
 
 
