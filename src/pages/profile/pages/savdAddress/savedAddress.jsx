c1d3479046934411809274cdec7a4c317dc7840d
djSomeone <gauravkumarprajapati496@gmail.com>
2025-01-10 17:36:47 +0530

    fiexd scroll wishlist

---
+++ src/pages/profile/pages/savdAddress/savedAddress.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+import './SavedAddress.css';
+
+const SavedAddressPage = () => {
+  const [addresses, setAddresses] = useState([
+    '123 Main Street, City, Country',
+    '456 Another Road, City, Country',
+    '',
+    '',
+    '',
+    '',
+  ]);
+
+  const handleEdit = (index) => {
+    const newAddress = prompt('Enter new address:', addresses[index]);
+    if (newAddress !== null) {
+      const updatedAddresses = [...addresses];
+      updatedAddresses[index] = newAddress;
+      setAddresses(updatedAddresses);
+    }
+  };
+
+  const handleAdd = () => {
+    const newAddress = prompt('Enter new address:');
+    if (newAddress) {
+      setAddresses([...addresses, newAddress]);
+    }
+  };
+
+  return (
+    <div className="saved-address-page">
+      <div className="header">
+        <h2>Saved Address</h2>
+        <AddAddressButton onAdd={handleAdd} />
+      </div>
+      <AddressList addresses={addresses} onEdit={handleEdit} />
+    </div>
+  );
+};
+
+export default SavedAddressPage;
